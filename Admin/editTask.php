3ec2daf7cee5a79b1124debb137bf9f3818c33f6
RUIXIN_H <76832192+RLirri@users.noreply.github.com>
2022-06-09 10:55:32 +0800

    temp
    simple modification, add map api, will edit after connecting database

---
+++ Admin/editTask.php
@@ -0,0 +1,8 @@
+<?php
+include '/Database/db_connect.php';
+$qry = $conn->query("SELECT * FROM project_list where id = ".$_GET['id'])->fetch_array();
+foreach($qry as $k => $v){
+	$$k = $v;
+}
+include 'newTask.php';
+?>
